b0b3a3ccdf9d70516ade93e3bc1e1dfe597b575d
n8ers <nathanraysheryak@gmail.com>
2019-10-19 10:49:03 -0400

    build portfolio components

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "../styles/PortfolioItem.css";
 
-const PortfolioItem = () => {
+const PortfolioItem = (props) => {
     return (
-        <div>
-            I am a portfolio project
+        <div className="portfilio-item-container">
+            <div>{props.title}</div>
+            <div className="portfilio-item-img-container">
+                <img className="portfilio-item-img" src={props.img} />
+            </div>
+            <div><p>{props.description}</p></div>
+            <div className="portfolio-item-btn-container">
+                <button><a target="_blank" href={props.githubLink}>GitHub</a></button>
+                <button><a target="_blank" href={props.liveLink}>live!</a></button>
+            </div>
         </div>
     )
 }
--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import PortfolioItem from "./PortfolioItem";
 import "../styles/PortfolioPage.css";
+import PlaceholderImg from "../img/placeholder.jpg";
 
 class PortfolioPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = [
+      { title: "electric weather", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
+      { title: "react todo", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
+      { title: "honey-dew", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
+      { title: "buds n brews", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
+      { title: "budgeter", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" }
+    ]
+  }
 
   render() {
     return (
       <div>
-        portfolio page
+        {
+          this.state.map((item) => {
+            return <PortfolioItem
+              title={item.title}
+              img={item.img}
+              description={item.description}
+              githubLink={item.githubLink}
+              liveLink={item.liveLink}
+            />
+          })
+        }
       </div >
     )
   }

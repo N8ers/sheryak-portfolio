192b381da463d6a191db469401dc7197536fa24f
n8ers <nathanraysheryak@gmail.com>
2019-10-21 19:47:25 -0400

    get portfolio into grid

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import "../styles/PortfolioItem.css";
 
 const PortfolioItem = (props) => {
-    return (
-        <div className="portfilio-item-container">
-            <div>{props.title}</div>
-            <div className="portfilio-item-img-container">
-                <img className="portfilio-item-img" src={props.img} />
-            </div>
-            <div><p>{props.description}</p></div>
-            <div className="portfolio-item-btn-container">
-                <button><a target="_blank" href={props.githubLink}>GitHub</a></button>
-                <button><a target="_blank" href={props.liveLink}>live!</a></button>
-            </div>
-        </div>
-    )
+	return (
+		<Grid item lg={4} md={4} sm={6} xs={12}>
+			<div className="portfilio-item-container">
+				<div className="portfilio-item-title">{props.title}</div>
+				<div className="portfilio-item-img-container">
+					<img className="portfilio-item-img" src={props.img} />
+				</div>
+				<div><p>{props.description}</p></div>
+				<div className="portfolio-item-btn-container">
+					<button className="portfolio-item-btn-item">
+						<a target="_blank" href={props.githubLink}>GitHub</a>
+					</button>
+					<button className="portfolio-item-btn-item">
+						<a target="_blank" href={props.liveLink}>live!</a>
+					</button>
+				</div>
+			</div>
+		</Grid>
+	)
 }
 
 export default PortfolioItem;
--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import PortfolioItem from "./PortfolioItem";
 import "../styles/PortfolioPage.css";
 import PlaceholderImg from "../img/placeholder.jpg";
@@ -9,29 +10,32 @@ class PortfolioPage extends React.Component {
     super(props);
 
     this.state = [
-      { title: "electric weather", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
+      { title: "electric weather", img: PlaceholderImg, description: "Run a weather app on your local computer, using Electron!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
       { title: "react todo", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
       { title: "honey-dew", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
       { title: "buds n brews", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
-      { title: "budgeter", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" }
+      { title: "budgeter", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" },
+      { title: "yelp API", img: PlaceholderImg, description: "This is a project!", githubLink: "https://github.com/N8ers", liveLink: "https://github.com/N8ers" }
     ]
   }
 
   render() {
     return (
-      <div>
-        {
-          this.state.map((item) => {
-            return <PortfolioItem
-              title={item.title}
-              img={item.img}
-              description={item.description}
-              githubLink={item.githubLink}
-              liveLink={item.liveLink}
-            />
-          })
-        }
-      </div >
+      <div className="portfilio-page">
+        <Grid container>
+          {
+            this.state.map((item) => {
+              return <PortfolioItem
+                title={item.title}
+                img={item.img}
+                description={item.description}
+                githubLink={item.githubLink}
+                liveLink={item.liveLink}
+              />
+            })
+          }
+        </Grid>
+      </div>
     )
   }
 
